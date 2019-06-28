a6ac270d20bc68a9c7abfedd12f8cf206479023c
Moe Ali <39111972+moyoonthego@users.noreply.github.com>
2019-06-28 01:30:16 -0400

    added background and header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 text-align="center">Maze Path Solver</h1>
+      <h3>Upload a maze image below and hit the
+       "start" button to begin processing the image. We will find the shortest path/solution to the maze.</h3>
     </div>
   );
+
 }
 
 export default App;
