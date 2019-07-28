543f3399dd107e246fbd5659b52e37d5ba6d6379
Moe Ali <39111972+moyoonthego@users.noreply.github.com>
2019-07-28 15:25:51 -0400

    done browser end code

--- src/App.js
+++ src/App.js
@@ -2,130 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  
-/*   let dropArea = document.getElementById('drop-area');
-  let filesDone = 0
-  let filesToDo = 0
-  let total = ['dragenter', 'dragover', 'dragleave', 'drop'];
-  let holdingitem = ['dragenter', 'dragover'];
-  let droppeditem = ['dragleave', 'drop'];
-
-   // Prevent default drag behaviors
-  total.forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false)   
-    document.body.addEventListener(eventName, preventDefaults, false)
-  })
-
-  // Highlight drop area when item is dragged over it
-  holdingitem.forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight, false)
-  })
-
-  droppeditem.forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight, false)
-  })
-
-  // Handle dropped files
-  dropArea.addEventListener('drop', handleDrop, false)
-
-  function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-
-  function highlight(e) {
-    dropArea.classList.add('highlight')
-  }
-
-  function unhighlight(e) {
-    dropArea.classList.remove('active')
-  }
-
-  function handleDrop(e) {
-    var dt = e.dataTransfer
-    var files = dt.files
-
-    handleFiles(files)
-  }
-
-  let uploadProgress = []
-  let progressBar = document.getElementById('progress-bar')
-
-  function initializeProgress(numFiles) {
-    progressBar.value = 0
-    uploadProgress = []
-
-    for(let i = numFiles; i > 0; i--) {
-      uploadProgress.push(0)
-    }
-  }
-
-  function updateProgress(fileNumber, percent) {
-    uploadProgress[fileNumber] = percent
-    let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-    console.debug('update', fileNumber, percent, total)
-    progressBar.value = total
-  }
-
-  function handleFiles(files) {
-    files = [...files]
-    initializeProgress(files.length)
-    files.forEach(uploadFile)
-    files.forEach(previewFile)
-  }
-
-  function previewFile(file) {
-    let reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onloadend = function() {
-      let img = document.createElement('img')
-      img.src = reader.result
-      document.getElementById('gallery').appendChild(img)
-    }
-  }
-
-  function uploadFile(file, i) {
-    var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
-    var xhr = new XMLHttpRequest()
-    var formData = new FormData()
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-    // Update progress (can be used to show progress indicator)
-    xhr.upload.addEventListener("progress", function(e) {
-      updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-    })
-
-    xhr.addEventListener('readystatechange', function(e) {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        updateProgress(i, 100) // <- Add this
-      }
-      else if (xhr.readyState === 4 && xhr.status !== 200) {
-        // Error. Inform the user
-      }
-    })
-
-    formData.append('upload_preset', 'ujpu6gyk')
-    formData.append('file', file)
-    xhr.send(formData)
-  } */
 
     return (
       <React.Fragment>
         <div>
           <h1 text-align="center">Maze Path Solver</h1>
-          <h3>Upload a maze image below and hit the "start" button to begin processing the image. We will find the shortest path/solution to the maze.</h3>
+          <h3>Upload a <b>single</b> maze image below and hit the "start" button to begin processing the image. We will find the shortest path/solution to the maze.</h3>
           <h3>Try downloading and entering a sample file to the right for an example of valid file format.</h3>
+          <h3>Refresh the page to start over or reset.</h3>
         </div>
         <div id="wrapper">
           <div id="drop-area">
             <form class="my-form">
               <p >Upload a file with the button below or by dragging and dropping images onto the dashed region</p>
-              <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)"></input>
-              <label class="button" for="fileElem">Select a maze/path-tracing file</label>
+              <input type="file" id="fileElem" accept="image/*" onchange="handleDrop(this.files)"></input>
+              <label class="button" id='selectionbutton' for="fileElem">Select a maze/path-tracing file</label>
             </form>
-            <progress id="progress-bar" max='100' value='0'></progress>
             <div id="gallery" />
+            <button class='button' id='solvebutton'>Solve</button>
           </div>
           <div id="sample-file-area">
             <form class="my-form-2">
--- src/index.js
+++ src/index.js
@@ -5,6 +5,120 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+let dropArea = document.getElementById('drop-area');
+let selectionbox = document.getElementById('fileElem');
+let selectionbutton = document.getElementById('selectionbutton');
+let files = [];
+let total = ['dragenter', 'dragover', 'dragleave', 'drop'];
+let holdingitem = ['dragenter', 'dragover'];
+let droppeditem = ['dragleave', 'drop'];
+
+selectionbox.onchange = e => { 
+    var file = e.target.files[0];
+    if (files.length !== 1) {
+        files= [file]
+        handleFiles(files)
+    }
+}
+document.getElementById('solvebutton').onclick = function() {
+    document.getElementById('solvebutton').textContent = 'Solving...'
+    uploadFile(files[0]);
+}
+
+  // Prevent default drag behaviors
+  total.forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)   
+    document.body.addEventListener(eventName, preventDefaults, false)
+  })
+
+  // Highlight drop area when item is dragged over it
+  holdingitem.forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+  })
+
+  droppeditem.forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+  })
+
+  // Handle dropped files
+  dropArea.addEventListener('drop', handleDrop, false)
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('active')
+  dropArea.classList.remove('highlight')
+}
+
+function handleDrop(e) {
+  var dt = e.dataTransfer
+  var cfiles = dt.files
+  if (files.length !== 1) {
+    if (cfiles.length === 1) {
+        files.push(cfiles[0])
+        handleFiles(files)
+    }
+  }
+
+}
+
+function handleFiles(files) {
+    if (files.length === 1) {
+        selectionbutton.textContent = "Selection complete"
+        files = [...files]
+        files.forEach(previewFile)
+    }
+}
+
+function previewFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('gallery').appendChild(img)
+  }
+}
+
+function uploadFile(file, i) {
+  /*
+  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
+  var xhr = new XMLHttpRequest()
+  var formData = new FormData()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+
+  // Update progress (can be used to show progress indicator)
+  xhr.upload.addEventListener("progress", function(e) {
+  })
+
+  xhr.addEventListener('readystatechange', function(e) {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    }
+    else if (xhr.readyState === 4 && xhr.status !== 200) {
+      // Error. Inform the user
+    }
+  })
+
+  formData.append('upload_preset', 'ujpu6gyk')
+  formData.append('file', file)
+  xhr.send(formData)
+  */
+ //var myfolder= new Folder("testing-images");
+ let reader = new FileReader();
+ reader.readAsDataURL(file);
+ console.log(reader.readAsText);
+ //reader.onload=(e)=> {
+   //  c
+ //}
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
